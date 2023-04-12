0ae5b1fa0a4e1c9f9d30805bce09e6e9000cce7c
Hamed Haghjo <hamedhaghjo@hotmail.com>
2023-04-12 14:40:35 +0200

    Update for vercel

---
+++ api/index.js
@@ -0,0 +1,83 @@
+/*
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors'); // Add this line
+const app = express();
+const server = http.createServer(app);
+
+
+//const server = require('http').Server(app);
+//const io = new Server(server);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*', // Allow any origin to connect for now
+    methods: ['GET', 'POST'],
+  },
+});
+const port = 3001;
+
+app.use(cors()); // Add this line
+app.use(express.json());
+app.use(express.static('public'));
+
+// Rest of the server code remains the same
+
+
+app.post('/send', (req, res) => {
+  console.log('Received message:', req.body.message);
+  io.emit('newMessage', req.body.message);
+  res.send('Message received.');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+});
+
+server.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+module.exports = (req, res) => {
+  if (req.method === 'POST') {
+    // Handle POST request from the C program
+    console.log('Received message:', req.body.message);
+    io.emit('newMessage', req.body.message);
+    res.send('Message received.');
+  } else {
+    // Handle other requests
+    res.status(405).send('Method Not Allowed');
+  }
+};
+*/
+
+// api/index.js
+
+const { Server } = require('socket.io');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = new Server(server);
+
+app.use(express.json());
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+});
+
+app.post('/send', (req, res) => {
+  console.log('Received message:', req.body.message);
+  io.emit('newMessage', req.body.message);
+  res.send('Message received.');
+});
+
+module.exports = (req, res) => {
+  if (req.method === 'POST') {
+    // Handle POST request from the C program
+    app(req, res);
+  } else {
+    // Handle other requests
+    res.status(405).send('Method Not Allowed');
+  }
+};
