7eb8348afb9eaca93a0a85b3946c12bbe282cf98
Hamed Haghjo <hamedhaghjo@hotmail.com>
2023-04-12 18:37:11 +0200

    Update C algo to reduce complexity in input

--- send_message.c
+++ send_message.c
@@ -1,38 +1,100 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#include <stdlib.h>
+#include <string.h>
 
-void send_message(const char *message) {
-  CURL *curl;
-  CURLcode res;
 
-  curl_global_init(CURL_GLOBAL_DEFAULT);
-  curl = curl_easy_init();
+char *escape_json_string(const char *input) {
+    int input_length = strlen(input);
+    int output_length = 0;
+    
+    const char *input_position;
+    char *output, *output_position;
+    
+    for (input_position = input; *input_position; ++input_position) {
+        if (*input_position == '\\' || *input_position == '"' || *input_position == '\n') {
+            output_length += 2;
+        } else {
+            output_length++;
+        }
+    }
 
-  if (curl) {
-    struct curl_slist *headers = NULL;
-    headers = curl_slist_append(headers, "Content-Type: application/json");
+    output = (char *)malloc((output_length + 1) * sizeof(char));
+    output_position = output;
 
-    curl_easy_setopt(curl, CURLOPT_URL, "http://bigunbot.myftp.org:3001/send");
-
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, message);
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
-    res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
-      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-    } else {
-      printf("Message sent successfully.\n");
+    for (input_position = input; *input_position; ++input_position) {
+        switch (*input_position) {
+            case '\\':
+            case '\"':
+                *output_position++ = '\\';
+                *output_position++ = *input_position;
+                break;
+            case '\n':
+                *output_position++ = '\\';
+                *output_position++ = 'n';
+                break;
+            default:
+                *output_position++ = *input_position;
+        }
     }
     
-    curl_easy_cleanup(curl);
-    curl_slist_free_all(headers);
-  }
+    *output_position = '\0';
 
-  curl_global_cleanup();
+    return output;
+}
+
+char *create_json_message(const char *input) {
+    char *escaped_input = escape_json_string(input);
+    int escaped_length = strlen(escaped_input);
+    int json_length = escaped_length + 15; // extra characters for JSON format
+
+    char *json_message = (char *)malloc((json_length + 1) * sizeof(char));
+    sprintf(json_message, "{\"message\": \"%s\"}", escaped_input);
+
+    free(escaped_input);
+
+    return json_message;
+}
+
+
+void send_message(const char *message) {
+    CURL *curl;
+    CURLcode res;
+
+    char *json_message = create_json_message(message);
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl) {
+        struct curl_slist *headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+
+        curl_easy_setopt(curl, CURLOPT_URL, "http://bigunbot.myftp.org:3001/send");
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_message);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        } else {
+            printf("Message sent successfully.\n");
+        }
+        
+        curl_easy_cleanup(curl);
+        curl_slist_free_all(headers);
+    }
+
+    curl_global_cleanup();
+    free(json_message);
 }
 
 int main(void) {
-  const char *message = "{\"message\": \"Hello Mr.Website from the C program! \\n What is going on? By making these changes, the message content should wrap to a new line when it exceeds the available width, without affecting the timestamp and line number elements.\"}";
+  
+  //const char *message = "{\"message\": \"Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere \\nlorem ipsum dolor sit amet consectetur adipiscing elit duis tristique\\nsollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum\"}";
+  const char *message = "Risus ultricies tristique nulla aliquet enim tortor at auctor urna nun \nid cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam\nultrices sagittis orci a scelerisque purus semper eget";
+  //const char *message = "pulvinar etiam non quam lacus suspendisse faucibus interdum posuere \nlorem ipsum dolor sit amet consectetur adipiscing elit duis tristique\nsollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum";
+
   send_message(message);
 
   return 0;
