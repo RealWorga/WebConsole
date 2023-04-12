74e2d9899ec65b4f0f26b71c37bff7ea635a1e10
Hamed Haghjo <hamedhaghjo@hotmail.com>
2023-04-12 14:55:12 +0200

    Add client folder and its contents

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { io } from 'socket.io-client';
+import './App.css';
+
+function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    //const socket = io('http://localhost:3001');
+    const socket = io('/');
+    socket.on('newMessage', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const clearConsole = () => {
+    setMessages([]);
+  };
+
+  return (
+    <div className="App">
+      <div className="console-container">
+        <h1 className="console-title">
+          Web Console made by{' '}
+          <a
+            href="https://github.com/RealWorga"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="github-link"
+          >
+            RealWorga
+          </a>
+        </h1>
+        <div className="console">
+          {messages.map((message, index) => (
+            <div key={index}>{message}</div>
+          ))}
+        </div>
+        <button onClick={clearConsole} className="refresh-button"> {/* Move the button here */}
+          Clear Console
+        </button>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
