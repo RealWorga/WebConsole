0671b7c2464c9bf8248ab8a30965f963bba9463b
Hamed Haghjo <hamedhaghjo@hotmail.com>
2023-04-12 16:56:07 +0200

    Remove cors

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const cors = require('cors'); // Add this line
+const cors = require('cors'); // Add this for local first usage
 const app = express();
 const server = http.createServer(app);
 
@@ -13,7 +13,7 @@ const io = new Server(server, {
 });
 const port = 3001;
 
-app.use(cors()); // Add this line
+//app.use(cors()); // Add for local first usage
 app.use(express.json());
 app.use(express.static('public'));
 
